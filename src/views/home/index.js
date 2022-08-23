dfadffea696aa43ea640d9f72d5df7efb9f6ef23
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-22 21:46:07 -0500

    enable connection with contract

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,32 @@
+import { useCallback, useEffect, useState } from 'react'
+import useHandleNfts from '../../hooks/useHandleNfts'
+import { useWeb3React } from '@web3-react/core'
+
 const Home = () => {
+  const { active } = useWeb3React()
+
+  const [maxSupply, setMaxSupply] = useState()
+
+  const virtualitoNFTs = useHandleNfts()
+
+  const getMaxSupply = useCallback(async () => {
+    if (virtualitoNFTs) {
+      const result = await virtualitoNFTs.methods.maxSuply().call()
+      setMaxSupply(result)
+    }
+  }, [virtualitoNFTs])
+
+  useEffect(() => {
+    getMaxSupply()
+  }, [getMaxSupply])
+
+  if (!active) {
+    return 'Conecta tu wallet'
+  }
+
   return (
     <>
-      <p>Home</p>
+      <p>Max Supply: {maxSupply}</p>
     </>
   )
 }
