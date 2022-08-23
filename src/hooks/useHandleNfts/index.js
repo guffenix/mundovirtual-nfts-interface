7df23332f206f4bd9cd98e2c4b292d1a70a0218e
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-22 21:44:12 -0500

    hook for handle nfts

---
+++ src/hooks/useHandleNfts/index.js
@@ -0,0 +1,15 @@
+import { useMemo } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import MundoVirtualPunk from '../../config/web3/artifacts/MundoVirtualPunk'
+
+const { address, abi } = MundoVirtualPunk
+
+const useHandleNfts = () => {
+  const { active, library, chainId } = useWeb3React()
+
+  return useMemo(() => {
+    if (active) return new library.eth.Contract(abi, address[chainId])
+  }, [active, chainId, library?.eth?.Contract])
+}
+
+export default useHandleNfts
