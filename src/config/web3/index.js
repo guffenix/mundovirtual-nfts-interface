1e1f0a85a2f3b588d7bc58b092f56064502dd760
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-22 21:43:24 -0500

    update networks

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,13 +1,6 @@
 import Web3 from 'web3'
 import { InjectedConnector } from '@web3-react/injected-connector'
-
-const Networks = {
-  MainNet: 1,
-  Ropsten: 3,
-  Rinkeby: 4,
-  Goerli: 5,
-  Kovan: 42,
-}
+import { Networks } from '../Networks'
 
 const connector = new InjectedConnector({
   supportedChainIds: [Networks.Rinkeby, Networks.Goerli],
