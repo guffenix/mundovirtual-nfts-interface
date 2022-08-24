459f0540175bd24d9e1b89b5c60c1f6839ed4cdb
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-24 00:03:27 -0500

    add link to nft

--- src/views/gallery/index.js
+++ src/views/gallery/index.js
@@ -4,6 +4,7 @@ import { Grid } from '@chakra-ui/react'
 import PunkCard from '../../components/punk-card'
 import Loading from '../../components/loading'
 import RequestAccess from '../../components/request-access'
+import { Link } from 'react-router-dom'
 
 const Gallery = () => {
   const { active } = useWeb3React()
@@ -20,7 +21,9 @@ const Gallery = () => {
       ) : (
         <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6}>
           {nfts.map(({ name, image, tokenId }) => (
-            <PunkCard key={tokenId} image={image} name={name} />
+            <Link key={tokenId} to={`/gallery/${tokenId}`}>
+              <PunkCard image={image} name={name} />
+            </Link>
           ))}
         </Grid>
       )}
