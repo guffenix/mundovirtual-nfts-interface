462af8e116385282c2f3993a67b157f573165b7d
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-26 01:20:06 -0500

    enable transfering function

--- src/views/card/index.js
+++ src/views/card/index.js
@@ -10,6 +10,7 @@ import {
   Tbody,
   Button,
   Tag,
+  useToast,
 } from '@chakra-ui/react'
 import { useWeb3React } from '@web3-react/core'
 import { useParams } from 'react-router-dom'
@@ -19,19 +20,62 @@ import { useNftData } from '../../hooks/useNftsData'
 import PunkCard from '../../components/punk-card'
 import RequestAccess from '../../components/request-access'
 import Loading from '../../components/loading'
+import { useState } from 'react'
+import useHandleNfts from '../../hooks/useHandleNfts'
 
 const Card = () => {
-  const { active, account } = useWeb3React()
+  const { active, account, library } = useWeb3React()
   const { tokenId } = useParams()
-  const { loading, nfts } = useNftData(tokenId)
+  const { loading, nfts, update } = useNftData(tokenId)
+  const toast = useToast()
+  const mundoVirtualNFT = useHandleNfts()
+  const [transfering, setTransfering] = useState(false)
+
+  const transfer = () => {
+    setTransfering(true)
+
+    const address = prompt('Ingresa la dirección: ')
+    const isAddress = library.utils.isAddress(address)
+
+    if (!isAddress) {
+      toast({
+        title: 'Dirección Inválida',
+        description: 'La dirección no es una dirección de Ethereum',
+        status: 'error',
+      })
+
+      setTransfering(false)
+    } else {
+      mundoVirtualNFT.methods
+        .safeTransferFrom(nfts.owner, address, nfts.tokenId)
+        .send({
+          from: account,
+        })
+        .on('error', () => {
+          setTransfering(false)
+        })
+        .on('transactionHash', (txHash) => {
+          toast({
+            title: 'Transacción enviada',
+            description: txHash,
+            status: 'info',
+          })
+        })
+        .on('receipt', () => {
+          setTransfering(false)
+          toast({
+            title: 'Transacción confirmada',
+            description: `Transacción exitosa, el nuevo dueño es ${address}`,
+            status: 'success',
+          })
+          update()
+        })
+    }
+  }
 
   if (!active) return <RequestAccess />
 
   if (loading) return <Loading />
-
-  if (nfts) {
-    console.log('->', nfts, tokenId)
-  }
 
   return (
     <Stack
@@ -48,7 +92,12 @@ const Card = () => {
           name={nfts.name}
           image={nfts.image}
         />
-        <Button disabled={account !== nfts.owner} colorScheme="green">
+        <Button
+          disabled={account !== nfts.owner}
+          colorScheme="green"
+          isLoading={transfering}
+          onClick={transfer}
+        >
           {account !== nfts.owner ? 'No eres el dueño' : 'Transferir'}
         </Button>
       </Stack>
