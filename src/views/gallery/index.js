6b43017af364fdb772a2130779fd4f3f0bb4b740
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-26 11:58:55 -0500

    search nfts

--- src/views/gallery/index.js
+++ src/views/gallery/index.js
@@ -1,14 +1,57 @@
 import { useWeb3React } from '@web3-react/core'
 import { useNftsData } from '../../hooks/useNftsData'
-import { Grid } from '@chakra-ui/react'
 import PunkCard from '../../components/punk-card'
 import Loading from '../../components/loading'
 import RequestAccess from '../../components/request-access'
-import { Link } from 'react-router-dom'
+import {
+  Grid,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  InputRightElement,
+  Button,
+  FormHelperText,
+  FormControl,
+} from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+import { useState } from 'react'
+import { useHistory, useLocation, Link } from 'react-router-dom'
 
 const Gallery = () => {
-  const { active } = useWeb3React()
-  const { loading, nfts } = useNftsData()
+  const { active, library } = useWeb3React()
+  const { search } = useLocation()
+  const { push } = useHistory()
+
+  const [submitted, setSubmitted] = useState(true)
+  const [validAddress, setValidAddress] = useState(true)
+  const [address, setAddress] = useState(
+    new URLSearchParams(search).get('address'),
+  )
+
+  const { loading, nfts } = useNftsData({
+    owner: submitted && validAddress ? address : null,
+  })
+
+  const handleAddressChange = ({ target: { value } }) => {
+    setAddress(value)
+    setSubmitted(false)
+    setValidAddress(false)
+  }
+
+  const submit = (event) => {
+    event.preventDefault()
+
+    if (address) {
+      const isValid = library.utils.isAddress(address)
+      setValidAddress(isValid)
+      setSubmitted(true)
+      if (isValid) {
+        push(`/gallery?address=${address}`)
+      }
+    } else {
+      push('/gallery')
+    }
+  }
 
   if (!active) {
     return <RequestAccess />
@@ -16,6 +59,30 @@ const Gallery = () => {
 
   return (
     <>
+      <form onSubmit={submit}>
+        <FormControl>
+          <InputGroup mb={3}>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<SearchIcon color="gray.300" />}
+            />
+            <Input
+              isInvalid={false}
+              value={address ?? ''}
+              onChange={handleAddressChange}
+              placeholder="Buscar por dirección"
+            />
+            <InputRightElement width="5.5rem">
+              <Button type="submit" h="1.75rem" size="sm">
+                Buscar
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          {submitted && !validAddress && (
+            <FormHelperText>Dirección inválida</FormHelperText>
+          )}
+        </FormControl>
+      </form>
       {loading ? (
         <Loading />
       ) : (
