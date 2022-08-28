8515d781ea0ae13f4a59bba94efff738bbf41383
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-27 21:58:12 -0500

    update components

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,11 +1,11 @@
-import { Center, Spinner } from "@chakra-ui/react";
+import { Center, Spinner } from '@chakra-ui/react'
 
 const Loading = () => {
   return (
     <Center my={20}>
       <Spinner />
     </Center>
-  );
-};
+  )
+}
 
-export default Loading;
+export default Loading
--- src/components/request-access/index.js
+++ src/components/request-access/index.js
@@ -4,17 +4,17 @@ import {
   AlertTitle,
   AlertDescription,
   CloseButton,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react'
 
 const RequestAccess = () => {
   return (
     <Alert status="error">
       <AlertIcon />
       <AlertTitle mr={2}>Conecta tu wallet</AlertTitle>
-      <AlertDescription>para acceder a la app</AlertDescription>
+      <AlertDescription>para acceder a esta zona.</AlertDescription>
       <CloseButton position="absolute" right="8px" top="8px" />
     </Alert>
-  );
-};
+  )
+}
 
-export default RequestAccess;
+export default RequestAccess
