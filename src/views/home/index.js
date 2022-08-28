6d5e9b583e8684b12f3ea9a9103e1be7343164ea
Luis Fernando Ushiña <guffenix@gmail.com>
2022-08-27 22:02:16 -0500

    update landing

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -8,7 +8,6 @@ import {
   Badge,
   useToast,
 } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
 import { useCallback, useEffect, useState } from 'react'
 import useHandleNfts from '../../hooks/useHandleNfts'
 import { useWeb3React } from '@web3-react/core'
@@ -80,14 +79,14 @@ const Home = () => {
     <Stack
       align={'center'}
       spacing={{ base: 8, md: 10 }}
-      py={{ base: 20, md: 28 }}
+      py={{ base: 10, md: 18 }}
       direction={{ base: 'column-reverse', md: 'row' }}
     >
       <Stack flex={1} spacing={{ base: 5, md: 10 }}>
         <Heading
           lineHeight={1.1}
           fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
+          fontSize={{ base: '3xl', sm: '4xl', lg: '5xl' }}
         >
           <Text
             as={'span'}
@@ -95,7 +94,7 @@ const Home = () => {
             _after={{
               content: "''",
               width: 'full',
-              height: '30%',
+              height: '25%',
               position: 'absolute',
               bottom: 1,
               left: 0,
@@ -103,22 +102,21 @@ const Home = () => {
               zIndex: -1,
             }}
           >
-            MundoVirtual NFT's
+            CypherPunk Avatar
           </Text>
         </Heading>
         <Text color={'gray.500'}>
-          MundoVirtual NFT's es una colección de Avatares randomizados cuya
-          metadata es almacenada on-chain. Poseen características únicas y sólo
-          hay{' '}
-          {maxStock > 0 ? ` ${maxStock} NFT's por mintear!` : `Se agotaron :(`}{' '}
-          en existencia.
+          Se trata de una colección de 10000 avatares únicos. Que representan
+          simbólicamente a la lista de los cypherpunks de mundovirtual. Para
+          rendir homenaje este proyecto trata de trata defender la privacidad
+          con la criptografía. con firmas digitales, smart contracts y on-chain.
         </Text>
 
+        <Text color={'gray.500'}>Qué esperas para ser parte cypherpunk?</Text>
         <Text color={'purple.500'}>
-          La semilla de generación está dada por un secuencial basado en tu
-          address. Usa el previsualizador para averiguar cuál sería el tuyo si
-          lo minteas en este momento
+          {maxStock > 0 ? `Solo quedan ${maxStock}` : ``}
         </Text>
+
         <Stack
           spacing={{ base: 4, sm: 6 }}
           direction={{ base: 'column', sm: 'row' }}
@@ -135,13 +133,8 @@ const Home = () => {
             onClick={mint}
             isLoading={isMinting}
           >
-            Obtén tu NFT
+            Quiero mi CypherPunk
           </Button>
-          <Link to="/gallery">
-            <Button rounded={'full'} size={'lg'} fontWeight={'normal'} px={6}>
-              Galería
-            </Button>
-          </Link>
         </Stack>
       </Stack>
       <Flex
